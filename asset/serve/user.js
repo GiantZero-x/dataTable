23c16dac87db4776109184613e50db16e7406b67
Gcx <18167999108@163.com>
2017-07-07 23:14:02 +0800

    change serve

---
+++ asset/serve/user.js
@@ -0,0 +1,15 @@
+const Mock = require('mockjs');
+const Users = [];
+
+for (let i = 0; i < 80; i++) {
+    Users.push(Mock.mock({
+        id: Mock.Random.increment(),
+        name: Mock.Random.cname(),
+        addr: Mock.mock('@county(true)'),
+        'age|18-60': 1,
+        birth: Mock.Random.date(),
+        sex: Mock.Random.character('男女')
+    }));
+}
+
+exports.Users =  Users;
